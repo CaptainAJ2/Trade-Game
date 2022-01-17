7f8096d635f29c4cccd65fa8352dbadd32d81cd4
CaptainAJ2 <95555497+CaptainAJ2@users.noreply.github.com>
2022-01-16 20:36:40 -0500

    Add files via upload

---
+++ Arcade Game/platformee/levels/level_data/brick_tiles.v1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="brick_tiles.v1" tilewidth="64" tileheight="64" tilecount="1" columns="1">
+ <image source="../../../../../Downloads/brick_tiles.v1.png" width="64" height="64"/>
+</tileset>
---
+++ Arcade Game/platformee/levels/tilesets/Forest TileSet Pixel Art #1 64x64.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Forest TileSet Pixel Art #1 64x64" tilewidth="64" tileheight="64" tilecount="36" columns="6">
+ <image source="Forest TileSet Pixel Art #1 64x64.png" width="384" height="384"/>
+</tileset>
